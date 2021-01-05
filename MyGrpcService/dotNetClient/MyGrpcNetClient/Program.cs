efa2072adbeee7ac89eed6702f523a3bc3ff28e4
jeremyliujin <jackyliujin@qq.com>
2021-01-05 22:55:48 +0800

    add dotnet consol app calls grpc service

---
+++ MyGrpcService/dotNetClient/MyGrpcNetClient/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using Greet;
+using Grpc.Net.Client;
+
+namespace MyGrpcNetClient
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var channel = GrpcChannel.ForAddress("https://localhost:5001");
+            var client = new Greeter.GreeterClient(channel);
+
+            var response = client.SayHello(new HelloRequest
+            {
+                Name = "gRPC"
+            });
+
+            Console.WriteLine(response.Message);
+
+            Console.ReadKey();
+        }
+    }
+}
